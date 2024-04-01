48256e78cc7d32e9420a3fcdb958984b1126d3cf
bob frankel <bob@biosbob.biz>
2024-04-01 04:58:36 -0700

    play

--- src/main.zig
+++ src/main.zig
@@ -4,5 +4,9 @@ const cli = @import("zig-cli");
 const yaml = @import("yaml");
 
 pub fn main() !void {
-    //    std.debug.print("\n{any}\n", .{cli.ColorUsage.auto});
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocr = arena.allocator();
+    const path = try std.fs.cwd().realpathAlloc(allocr, ".");
+    std.debug.print("path = {s}\n", .{path});
 }
