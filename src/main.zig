f69343fb794d6bb2dccdf9bcc8574956bc4abe0e
bob frankel <bob@biosbob.biz>
2024-04-01 05:49:02 -0700

    heap.zig

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,13 @@
 const std = @import("std");
 
 const cli = @import("zig-cli");
-const yaml = @import("yaml");
 
-pub fn main() !void {}
+const heap = @import("./heap.zig");
+
+pub fn main() !void {
+    defer heap.deinit();
+
+    const allctr = heap.get();
+    const path = try std.fs.cwd().realpathAlloc(allctr, ".");
+    std.debug.print("path = {s}\n", .{path});
+}
