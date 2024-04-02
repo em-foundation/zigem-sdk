be0c1ef9d41f353e49f1191e29a172b0e939cd91
bob frankel <bob@biosbob.biz>
2024-04-02 05:20:25 -0700

    Session: WIP

--- src/Path.zig
+++ src/Path.zig
@@ -6,6 +6,10 @@ pub fn delete(abs_path: []const u8) void {
     std.fs.deleteTreeAbsolute(abs_path) catch return;
 }
 
+pub fn dirname(path: []const u8) []const u8 {
+    return if (std.fs.path.dirname(path)) |dn| dn else "";
+}
+
 pub fn join(paths: []const []const u8) []const u8 {
     const res = std.fs.path.join(Heap.get(), paths) catch unreachable;
     return res;
--- src/Session.zig
+++ src/Session.zig
@@ -10,15 +10,17 @@ pub const Mode = enum {
 
 var cur_bpath: []const u8 = undefined;
 var cur_mode: Mode = undefined;
-var cur_out_root: []const u8 = undefined;
+var out_root: []const u8 = undefined;
+var work_root: []const u8 = undefined;
 
 pub fn activate(bundle: []const u8, mode: Mode, _: ?[]const u8) !void {
     cur_bpath = try Path.normalize(bundle);
     cur_mode = mode;
-    const a = [_][]const u8{ cur_bpath, ".out" };
-    cur_out_root = Path.join(&a);
+    out_root = Path.join(&.{ cur_bpath, ".out" });
+    work_root = Path.dirname(cur_bpath);
+    std.log.debug("work = {s}", .{work_root});
     if (mode == .CLEAN) {
-        Path.delete(cur_out_root);
+        Path.delete(out_root);
         return;
     }
 }
