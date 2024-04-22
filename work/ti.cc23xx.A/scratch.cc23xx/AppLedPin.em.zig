b904f781a5faec337419983c1a324919734c5331
bob frankel <bob@biosbob.biz>
2024-04-21 17:21:14 -0700

    remove "private" configs

--- work/ti.cc23xx.A/scratch.cc23xx/AppLedPin.em.zig
+++ work/ti.cc23xx.A/scratch.cc23xx/AppLedPin.em.zig
@@ -10,29 +10,19 @@ pub const Hal = em.import.@"ti.mcu.cc23xx/Hal";
 
 pub var c_pin = em__unit.declareConfig("pin", i16){};
 
-pub var _is_def = em__unit.declareConfig("_is_def", bool){};
-pub var _mask = em__unit.declareConfig("_mask", u32){};
-
 pub fn em__initH() void {
     c_pin.initH(-1);
-    _mask.initH(0);
-    _is_def.initH(false);
-}
-
-pub fn em__constructH() void {
-    const p = c_pin.get();
-    if (p < 0) return;
-    _is_def.set(true);
-    const p5 = @as(u5, @bitCast(@as(i5, @truncate(p))));
-    const m: u32 = @as(u32, 1) << p5;
-    _mask.set(m);
 }
 
 const REG = em.REG;
 
 const pin = c_pin.unwrap();
-const is_def = _is_def.unwrap();
-const mask = _mask.unwrap();
+const is_def = (pin >= 0);
+const mask = init: {
+    const p5 = @as(u5, @bitCast(@as(i5, @truncate(pin))));
+    const m: u32 = @as(u32, 1) << p5;
+    break :init m;
+};
 
 pub fn clear() void {
     if (is_def) REG(Hal.GPIO_BASE + Hal.GPIO_O_DOUTCLR31_0).* = mask;
