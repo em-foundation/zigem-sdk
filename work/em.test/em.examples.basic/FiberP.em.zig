57ba2e2c26955073ad57ef9fe8f52dc9e4a4bb1e
bob frankel <bob@biosbob.biz>
2024-07-06 13:57:28 -0700

    FiberP: em__C

--- work/em.test/em.examples.basic/FiberP.em.zig
+++ work/em.test/em.examples.basic/FiberP.em.zig
@@ -1,23 +1,26 @@
 pub const em = @import("../../.gen/em.zig");
 pub const em__unit = em.Module(@This(), .{});
+pub const em__C: *EM__CONFIG = em__unit.Config(EM__CONFIG);
 
 pub const AppLed = em.Import.@"em__distro/BoardC".AppLed;
 pub const Common = em.Import.@"em.mcu/Common";
 pub const FiberMgr = em.Import.@"em.utils/FiberMgr";
 
-pub const c_blinkF = em__unit.config("blinkF", em.Ptr(FiberMgr.Fiber));
+pub const EM__CONFIG = struct {
+    blinkF: em.Param(em.Ptr(FiberMgr.Fiber)),
+};
 
 pub const EM__HOST = struct {
     //
     pub fn em__constructH() void {
         const fiber = FiberMgr.createH(em__unit.func("blinkFB", em.CB(FiberMgr.FiberBody)));
-        c_blinkF.set(fiber);
+        em__C.blinkF.set(fiber);
     }
 };
 
 pub const EM__TARG = struct {
     //
-    const blinkF = c_blinkF.unwrap();
+    const blinkF = em__C.blinkF.unwrap();
 
     pub fn em__run() void {
         blinkF.post();
