f572ece4451a1817a17226427d32e6d42258a614
bob frankel <bob@biosbob.biz>
2024-07-07 17:52:00 -0700

    Utils: em__C, em.Array

--- work/em.bench/em.coremark/Utils.em.zig
+++ work/em.bench/em.coremark/Utils.em.zig
@@ -1,5 +1,6 @@
 pub const em = @import("../../.gen/em.zig");
 pub const em__unit = em.Module(@This(), .{});
+pub const em__C: *EM__CONFIG = em__unit.Config(EM__CONFIG);
 
 pub const NUM_SEEDS: usize = 5;
 
@@ -8,23 +9,25 @@ pub const Kind = enum { FINAL, LIST, MATRIX, STATE, ZZZ_ };
 pub const seed_t = u16;
 pub const sum_t = u16;
 
-pub var seed_tab = em__unit.array("seed_tab", seed_t);
+pub const EM__CONFIG = struct {
+    seed_tab: em.Array(seed_t),
+};
 
 pub const EM__HOST = struct {
     //
     pub fn em__initH() void {
-        seed_tab.setLen(NUM_SEEDS);
+        em__C.seed_tab.setLen(NUM_SEEDS);
     }
 
     pub fn bindSeedH(idx: u8, val: seed_t) void {
-        seed_tab.unwrap()[idx - 1] = val;
+        em__C.seed_tab.elems()[idx - 1] = val;
     }
 };
 
 pub const EM__TARG = struct {
     //
     var crc_tab = em.std.mem.zeroes([@intFromEnum(Kind.ZZZ_)]sum_t);
-    var v_seed_tab = seed_tab.unwrap();
+    const seed_tab = em__C.seed_tab.unwrap();
 
     pub fn bindCrc(kind: Kind, crc: sum_t) void {
         const p = &crc_tab[@intFromEnum(kind)];
@@ -36,7 +39,7 @@ pub const EM__TARG = struct {
     }
 
     pub fn getSeed(idx: u8) seed_t {
-        const p: *volatile u16 = @constCast(&v_seed_tab[idx - 1]);
+        const p: *volatile u16 = @constCast(&seed_tab[idx - 1]);
         return p.*;
     }
 
--- work/em.core/em.lang/em.zig
+++ work/em.core/em.lang/em.zig
@@ -838,6 +838,58 @@ pub fn Module2(This: type, opts: UnitOpts) Unit {
     };
 }
 
+pub fn Array(T: type) type {
+    return if (DOMAIN == .HOST) Array_H(T) else Array_T(T);
+}
+
+pub fn Array_H(T: type) type {
+    return struct {
+        const Self = @This();
+
+        pub const _em__builtin = {};
+
+        _list: std.ArrayList(T) = std.ArrayList(T).init(arena.allocator()),
+
+        pub fn addElem(self: *Self, elem: T) void {
+            self._list.append(elem) catch fail();
+        }
+
+        pub fn elems(self: *Self) []T {
+            return self._list.items;
+        }
+
+        pub fn setLen(self: *Self, len: usize) void {
+            const l = self._list.items.len;
+            if (len > l) {
+                for (l..len) |_| {
+                    self.addElem(std.mem.zeroes(T));
+                }
+            }
+        }
+
+        pub fn toString(self: Self) []const u8 {
+            const tn = mkTypeName(T);
+            var sb = StringH{};
+            sb.add(sprint("[_]{s}{{", .{tn}));
+            for (self._list.items) |e| {
+                sb.add(sprint("    {s},\n", .{toStringAux(e)}));
+            }
+            sb.add("}");
+            return sprint("em.Array_T({s}){{ ._a = @constCast(&{s})}}", .{ tn, sb.get() });
+        }
+    };
+}
+
+pub fn Array_T(T: type) type {
+    return struct {
+        const Self = @This();
+        _a: []T,
+        pub fn unwrap(self: Self) []T {
+            return self._a;
+        }
+    };
+}
+
 pub fn Factory(T: type) type {
     return if (DOMAIN == .HOST) Factory_H(T) else Factory_T(T);
 }
