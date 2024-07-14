36451bf899489744681d5b281181b03f9e2694db
bob frankel <bob@biosbob.biz>
2024-07-14 07:28:38 -0700

    LedT:  em__C

--- work/em.core/em.utils/LedT.em.zig
+++ work/em.core/em.utils/LedT.em.zig
@@ -1,29 +1,36 @@
 pub const em = @import("../../.gen/em.zig");
 pub const em__unit = em.Template(@This(), .{});
 
+pub const EM__CONFIG = struct {
+    Pin: em.Proxy(em.Import.@"em.hal/GpioI"),
+    active_low: em.Param(bool),
+};
+
 pub fn em__generateS(comptime name: []const u8) type {
     return struct {
+        //
         pub const em__unit = em.Module(@This(), .{
             .generated = true,
             .name = name,
         });
+        pub const em__C: *EM__CONFIG = @This().em__unit.Config(EM__CONFIG);
 
         pub const Poller = em.Import.@"em.mcu/Poller";
 
-        pub const c_active_low = @This().em__unit.config("active_low", bool);
-        pub const x_Pin = @This().em__unit.proxy("Pin", em.Import.@"em.hal/GpioI");
+        pub const c_active_low = em__C.active_low.ref();
+        pub const x_Pin = em__C.Pin.ref();
 
         pub const EM__HOST = struct {
             //
             pub fn em__initH() void {
-                c_active_low.init(false);
+                c_active_low.set(false);
             }
         };
 
         pub const EM__TARG = struct {
             //
-            const active_low = c_active_low.unwrap();
-            const Pin = x_Pin.unwrap();
+            const active_low = em__C.active_low.unwrap();
+            const Pin = em__C.Pin.unwrap();
 
             pub fn em__startup() void {
                 Pin.makeOutput();
