c0fa460fe20582ae29348cdd8f56c5f8c4ae82ce
bob frankel <bob@biosbob.biz>
2024-07-14 20:05:52 -0700

    Common:  em__C

--- work/em.core/em.mcu/Common.em.zig
+++ work/em.core/em.mcu/Common.em.zig
@@ -1,27 +1,33 @@
 pub const em = @import("../../.gen/em.zig");
 pub const em__unit = em.Module(@This(), .{});
+pub const em__C: *EM__CONFIG = em__unit.Config(EM__CONFIG);
 
-pub const x_BusyWait = em__unit.proxy("BusyWait", em.Import.@"em.hal/BusyWaitI");
-pub const BusyWait = x_BusyWait.unwrap();
+pub const EM__CONFIG = struct {
+    BusyWait: em.Proxy(em.Import.@"em.hal/BusyWaitI"),
+    ConsoleUart: em.Proxy(em.Import.@"em.hal/ConsoleUartI"),
+    GlobalInterrupts: em.Proxy(em.Import.@"em.hal/GlobalInterruptsI"),
+    Idle: em.Proxy(em.Import.@"em.hal/IdleI"),
+    Mcu: em.Proxy(em.Import.@"em.hal/McuI"),
+    MsCounter: em.Proxy(em.Import.@"em.hal/MsCounterI"),
+    UsCounter: em.Proxy(em.Import.@"em.hal/UsCounterI"),
+};
 
-pub const x_ConsoleUart = em__unit.proxy("ConsoleUart", em.Import.@"em.hal/ConsoleUartI");
-pub const ConsoleUart = x_ConsoleUart.unwrap();
+pub const EM__HOST = struct {
+    pub const x_BusyWait = em__C.BusyWait.ref();
+    pub const x_ConsoleUart = em__C.ConsoleUart.ref();
+    pub const x_GlobalInterrupts = em__C.GlobalInterrupts.ref();
+    pub const x_Idle = em__C.Idle.ref();
+    pub const x_Mcu = em__C.Mcu.ref();
+    pub const x_MsCounter = em__C.MsCounter.ref();
+    pub const x_UsCounter = em__C.UsCounter.ref();
+};
 
-pub const x_GlobalInterrupts = em__unit.proxy("GlobalInterrupts", em.Import.@"em.hal/GlobalInterruptsI");
-pub const GlobalInterrupts = x_GlobalInterrupts.unwrap();
-
-pub const x_Idle = em__unit.proxy("Idle", em.Import.@"em.hal/IdleI");
-pub const Idle = x_Idle.unwrap();
-
-pub const x_Mcu = em__unit.proxy("Mcu", em.Import.@"em.hal/McuI");
-pub const Mcu = x_Mcu.unwrap();
-
-pub const x_MsCounter = em__unit.proxy("MsCounter", em.Import.@"em.hal/MsCounterI");
-pub const MsCounter = x_MsCounter.unwrap();
-
-pub const x_UsCounter = em__unit.proxy("UsCounter", em.Import.@"em.hal/UsCounterI");
-pub const UsCounter = x_UsCounter.unwrap();
-
-pub const EM__HOST = struct {};
-
-pub const EM__TARG = struct {};
+pub const EM__TARG = struct {
+    pub const BusyWait = em__C.BusyWait.unwrap();
+    pub const ConsoleUart = em__C.ConsoleUart.unwrap();
+    pub const GlobalInterrupts = em__C.GlobalInterrupts.unwrap();
+    pub const Idle = em__C.Idle.unwrap();
+    pub const Mcu = em__C.Mcu.unwrap();
+    pub const MsCounter = em__C.MsCounter.unwrap();
+    pub const UsCounter = em__C.UsCounter.unwrap();
+};
