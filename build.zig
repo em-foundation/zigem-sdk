f2822ed05ad2c1283a5e777363c07a2b3a9c23da
bob frankel <bob@biosbob.biz>
2024-09-01 11:41:13 -0700

    Squashed commit of the following:
    commit 88e5dd4230aeb7dc8ae2be6e827edd3cf71ef06a
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sun Sep 1 11:39:16 2024 -0700
    
        zig-em cli => zigem cli
    
    commit 812a8a211e6d6fcbb581be3a1705bb4b68ec603d
    Merge: cbe08c8 874489a
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sun Sep 1 10:13:27 2024 -0700
    
        Merge branch 'bob-work' of github.com:em-foundation/zig-em-dev into bob-work
    
        fix divergent branches
    
    commit cbe08c843d5ff2b476387396a2a8959287ab44a8
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sun Sep 1 10:12:43 2024 -0700
    
        Session: minor fix
    
    commit b425c96adb76031425df2dcfbf37f6b990a94d89
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sun Sep 1 10:07:09 2024 -0700
    
        Merge branch 'bob-work' of github.com:em-foundation/zig-em-dev into bob-work
    
    commit 5011a714a60a85fd1f2fa3d4e37c999aa193a9c2
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sun Sep 1 10:04:31 2024 -0700
    
        Session: clean legacy
    
    commit 874489a08dd54ab95dcbdf3fb12cecfd16db9719
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sun Sep 1 10:03:29 2024 -0700
    
        add back em.build
    
    commit 78fd4b09706e6b9eb9c948d2cfbcb2aaf074c11b
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sun Sep 1 10:03:00 2024 -0700
    
        remove em.bench
    
    commit 850e4ee1e3f4fe81fb0984246cbd597abaa2d06d
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sun Sep 1 09:49:00 2024 -0700
    
        remove em.bench
    
    commit 660dd21207bae91d57285b34cfb025fcbf0fd1af
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sun Sep 1 09:47:56 2024 -0700
    
        .gitignore
    
    commit 94f2fda576333c18301bbc693689dddab43cbec5
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sun Sep 1 09:36:03 2024 -0700
    
        stash em.bench, ti.radio onto bob-work-xtra branch
    
    commit 0f9db02701b835083c7e1e42901cedd45ad3da18
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sun Sep 1 06:32:33 2024 -0700
    
        zigem/gen => zigem/
    
    commit eb99e7e596e87c2ba3dddebb1c42992132f8e089
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sat Aug 31 18:50:16 2024 -0700
    
        build/ => zigem/
    
    commit 2a04cffd9122f61571bac87deb03683604360ca0
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sat Aug 31 18:45:43 2024 -0700
    
        .out => out
    
    commit 4084a566556b8060a49224e71cbb45d9e6a53372
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sat Aug 31 18:41:42 2024 -0700
    
        .gen/ => gen/
    
    commit 7649490fb607f6b0046fae4a7240be10a211ac6f
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sat Aug 31 18:29:47 2024 -0700
    
        .zigem-main.zig
    
    commit fa23bb9266f957b1785122c4c0b29d05e64a6ebc
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sat Aug 31 16:57:32 2024 -0700
    
        nit
    
    commit b87f0b85adf00c12d3f0da35bbd8644362f453bd
    Author: bob frankel <bob@biosbob.biz>
    Date:   Sat Aug 31 16:53:43 2024 -0700
    
        build folder

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const target = b.host;
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
-        .name = "zig-em",
+        .name = "zigem",
         .root_source_file = std.Build.path(b, "src/main.zig"),
         .target = target,
         .optimize = optimize,
@@ -45,13 +45,13 @@ pub fn build(b: *std.Build) void {
     const verify_exe = b.addRunArtifact(exe);
     verify_exe.setCwd(std.Build.LazyPath{ .src_path = .{ .owner = b, .sub_path = "workspace" } });
     verify_exe.addArgs(&.{ "compile", "-u", "em.test/em.examples.basic/BlinkerP.em.zig" });
-    const verify_step = b.step("verify", "Verify zig-em");
+    const verify_step = b.step("verify", "Verify ZigEM");
     verify_step.dependOn(&verify_exe.step);
 
     const zigem_exe = b.addRunArtifact(exe);
     if (b.args) |args| zigem_exe.addArgs(args);
     zigem_exe.setCwd(std.Build.LazyPath{ .src_path = .{ .owner = b, .sub_path = "workspace" } });
-    const zigem_step = b.step("zig-em", "Execute the ZigEM CLI");
+    const zigem_step = b.step("zigem", "Execute the ZigEM CLI");
     zigem_step.dependOn(&zigem_exe.step);
 }
 
