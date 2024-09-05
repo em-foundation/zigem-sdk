8d63d63244498862e2170f9ce7fc5cfe9241adac
bob frankel <bob@biosbob.biz>
2024-09-05 11:02:46 -0700

    GlobalInterrupts: get_PRIMASK fix

--- workspace/em.arch/em.arch.arm/GlobalInterrupts.em.zig
+++ workspace/em.arch/em.arch.arm/GlobalInterrupts.em.zig
@@ -18,7 +18,6 @@ pub const EM__TARG = struct {
 
     pub fn enable() void {
         asm volatile ("cpsie i" ::: "memory");
-        // set_PRIMASK(0);
     }
 
     pub fn isEnabled() bool {
@@ -30,21 +29,9 @@ pub const EM__TARG = struct {
     }
 
     pub fn get_PRIMASK() u32 {
-        const key: u32 = 0;
-        asm volatile (
-            \\mrs %[key], primask        
-            :
-            : [key] "r" (key),
-            : "memory"
-        );
-        return key;
-    }
-
-    pub fn set_PRIMASK(m: u32) void {
-        asm volatile ("msr primask, %[m]"
-            :
-            : [m] "r" (m),
-            : "memory"
+        return asm volatile (
+            \\mrs %[ret], primask        
+            : [ret] "={r0}" (-> u32),
         );
     }
 };
