162d5c23fa4404e2fb0428a0722eb9a93adebc74
Geoffrey Cohler <g.cohler@computer.org>
2024-09-19 05:50:16 -0400

    Add clean script, update install.md (#14)

---
+++ clean.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+deep="-d"
+deepLong="--deep"
+clean=$1
+
+if [ "$clean" != "" ] && [ "$clean" != "$deep" ] && [ "$clean" != "$deepLong" ]; then
+    echo "usage:  clean.sh [$deep, $deepLong]"
+    echo default clean is zig-em workspace only
+    echo $deep or $deepLong cleans zig-build artifacts and zig global cache as well
+    exit 1
+fi
+
+echo clean workspace
+rm -rf $SCRIPT_DIR/workspace/{zigem,.zigem-main.zig}
+
+if [ "$clean" != "" ] ; then
+    echo clean zig build artifacts
+    rm -rf $SCRIPT_DIR/{.zig-cache,zig-out,zigem}
+
+    echo clean zig global cache
+    rm -rf $HOME/.cache/zig
+fi
