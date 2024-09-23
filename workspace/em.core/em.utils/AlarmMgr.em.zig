2851c6be3cfe9e71a8f90d3c82448709869dbd4d
bob frankel <bob@biosbob.biz>
2024-09-23 04:47:52 -0700

    AlarmMgr: DEBUG

--- workspace/em.core/em.utils/AlarmMgr.em.zig
+++ workspace/em.core/em.utils/AlarmMgr.em.zig
@@ -16,7 +16,8 @@ pub const Alarm = struct {
     const Self = @This();
     _fiber: FiberMgr.Obj,
     _thresh: u32 = 0, // time of alarm
-    _ticks: u32 = 0, // ticks remaining until alarm (0 == alarm inactive)
+    _dticks: u32 = 0, // delta ticks until alarm (0 == alarm inactive)
+    _idx: u8,
     pub fn active(self: *Self) bool {
         em__U.scope().Alarm_active(self);
     }
@@ -35,8 +36,11 @@ pub const EM__META = struct {
     //
     pub const WakeupTimer = em__C.WakeupTimer;
 
+    var cur_idx: u8 = 0;
+
     pub fn createH(fiber: FiberMgr.Obj) Obj {
-        const alarm = em__C.AlarmOF.createH(.{ ._fiber = fiber });
+        cur_idx += 1;
+        const alarm = em__C.AlarmOF.createH(.{ ._fiber = fiber, ._idx = cur_idx });
         return alarm;
     }
 };
@@ -54,33 +58,38 @@ pub const EM__TARG = struct {
         var max_ticks = ~@as(u32, 0); // largest u32
         for (0..alarm_tab.len) |idx| {
             var a = &alarm_tab[idx];
-            a._ticks -|= delta_ticks;
-            if (a._ticks > 0 and a._ticks < max_ticks) {
+            a._dticks -|= delta_ticks;
+            if (a._dticks > 0 and a._dticks < max_ticks) {
                 nxt_alarm = a;
-                max_ticks = a._ticks;
+                max_ticks = a._dticks;
             }
         }
         cur_alarm = nxt_alarm;
-        if (cur_alarm != null) {
-            WakeupTimer.enable(cur_alarm.?._thresh, &wakeupHandler);
+        if (cur_alarm) |ca| {
+            em.@"%%[a:]"(1);
+            em.@"%%[>]"(ca._idx);
+            em.@"%%[>]"(ca._thresh);
+            WakeupTimer.enable(ca._thresh, &wakeupHandler);
         }
     }
 
     fn wakeupHandler(_: WakeupTimer.HandlerArg) void {
+        em.@"%%[a]"();
+        em.@"%%[>]"(cur_alarm.?._idx);
         const alarm_tab = em__C.AlarmOF;
         const thresh: u32 = cur_alarm.?._thresh;
         for (0..alarm_tab.len) |idx| {
             var a = &alarm_tab[idx];
-            if (a._ticks > 0 and thresh == a._thresh) {
-                a._ticks = 0;
+            if (a._dticks > 0 and thresh == a._thresh) {
+                a._dticks = 0;
                 a._fiber.post(); // ring the alarm
             }
         }
-        findNextAlarm(cur_alarm.?._ticks);
+        findNextAlarm(cur_alarm.?._dticks);
     }
 
     pub fn Alarm_cancel(alarm: *Alarm) void {
-        alarm._ticks = 0;
+        alarm._dticks = 0;
         findNextAlarm(0);
     }
 
@@ -90,7 +99,11 @@ pub const EM__TARG = struct {
 
     fn Alarm_setup(alarm: *Alarm, ticks: u32) void {
         alarm._thresh = WakeupTimer.ticksToThresh(ticks);
-        alarm._ticks = ticks;
+        alarm._dticks = ticks;
+        //em.@"%%[a:]"(2);
+        //em.@"%%[>]"(alarm._idx);
+        //em.@"%%[>]"(alarm._dticks);
+        //em.@"%%[>]"(alarm._thresh);
         findNextAlarm(0);
     }
 
@@ -104,6 +117,8 @@ pub const EM__TARG = struct {
         const et_secs = EpochTime.getCurrent(&et_subs);
         const et_ticks = WakeupTimer.timeToTicks(et_secs, et_subs);
         const ticks = WakeupTimer.secs256ToTicks(secs256);
-        Alarm_setup(alarm, ticks - (et_ticks % ticks));
+        const rem = et_ticks % ticks;
+        Alarm_setup(alarm, ticks - rem);
+        // em.print("ticks = {d}, et_ticks = {d} , rem = {d}\n", .{ ticks, et_ticks, rem });
     }
 };
