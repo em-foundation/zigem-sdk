c4a1284c6a8b7e5256ea9da1b0c56b7a79992a8b
bob frankel <bob@biosbob.biz>
2024-10-11 14:14:03 -0700

    _{field}

--- workspace/em.core/em.utils/FiberMgr.em.zig
+++ workspace/em.core/em.utils/FiberMgr.em.zig
@@ -19,9 +19,9 @@ pub const FiberBody = struct {
 };
 
 pub const Fiber = struct {
-    link: ?Obj,
-    body: BodyFxn,
-    arg: usize = 0,
+    _link: ?Obj,
+    _body: BodyFxn,
+    _arg: usize = 0,
     pub fn post(self: *Fiber) void {
         EM__TARG.Fiber_post(self);
     }
@@ -54,15 +54,15 @@ pub const EM__TARG = struct {
             if (self.empty()) {
                 self.head = elem;
             } else {
-                self.tail.link = elem;
+                self.tail._link = elem;
             }
             self.tail = elem;
-            elem.link = END;
+            elem._link = END;
         }
         fn take(self: *Self) *Fiber {
             const e = self.head;
-            self.head = e.link.?;
-            e.link = null;
+            self.head = e._link.?;
+            e._link = null;
             if (self.head == END) self.tail = END;
             return e;
         }
@@ -71,9 +71,9 @@ pub const EM__TARG = struct {
     fn dispatch() void {
         while (!ready_list.empty()) {
             const fiber = ready_list.take();
-            const body = fiber.body;
+            const body = fiber._body;
             Common.GlobalInterrupts.enable();
-            body.?(.{ .arg = fiber.arg });
+            body.?(.{ .arg = fiber._arg });
             _ = Common.GlobalInterrupts.disable();
         }
     }
@@ -90,7 +90,7 @@ pub const EM__TARG = struct {
 
     fn Fiber_post(self: *Fiber) void {
         const key = Common.GlobalInterrupts.disable();
-        if (self.link == null) ready_list.give(self);
+        if (self._link == null) ready_list.give(self);
         Common.GlobalInterrupts.restore(key);
     }
 };
