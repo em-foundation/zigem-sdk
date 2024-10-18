02760d50b72070f69bf97c485bcf626c77e72397
bob frankel <bob@biosbob.biz>
2024-10-17 17:11:54 -0700

    Publisher: WIP

--- src/Publisher.zig
+++ src/Publisher.zig
@@ -4,10 +4,14 @@ const Fs = @import("./Fs.zig");
 const Heap = @import("./Heap.zig");
 const Out = @import("./Out.zig");
 
+const Ast = std.zig.Ast;
 const print = std.debug.print;
 
-var ast: std.zig.Ast = undefined;
+var ast: Ast = undefined;
 var file: *Out.File = undefined;
+var lines: std.ArrayList([]const u8) = undefined;
+var members: []const Ast.Node.Index = undefined;
+var tab: []const u8 = undefined;
 
 pub fn exec(path: []const u8, force: bool) !void {
     const norm = try Fs.normalize(path);
@@ -15,8 +19,9 @@ pub fn exec(path: []const u8, force: bool) !void {
     const mark = std.mem.indexOf(u8, txt, "//->>");
     const src = if (mark) |m| txt[0..m] else txt;
     const srcZ = try std.fmt.allocPrintZ(Heap.get(), "{s}", .{src});
-    ast = try std.zig.Ast.parse(Heap.get(), srcZ, .zig);
+    ast = try Ast.parse(Heap.get(), srcZ, .zig);
     const ren_src = try ast.render(Heap.get());
+    lines = try mkLines(ren_src);
     var digest: [32]u8 = undefined;
     std.crypto.hash.sha2.Sha256.hash(ren_src, &digest, .{});
     const hbuf = std.fmt.bytesToHex(digest, .lower);
@@ -30,25 +35,37 @@ pub fn exec(path: []const u8, force: bool) !void {
         done = !force and std.mem.eql(u8, &hbuf, suf2);
     }
     file = try Out.open(norm);
+    const gen_idx = findDecl("em__generateS");
+    if (gen_idx > 0) {
+        const gen_fn = ast.nodes.get(gen_idx);
+        const gen_body = ast.nodes.get(gen_fn.data.rhs);
+        const gen_blk = ast.nodes.get(gen_body.data.lhs);
+        members = ast.containerDecl(gen_blk.data.lhs).ast.members;
+        tab = "        ";
+    } else {
+        members = ast.rootDecls();
+        tab = "";
+    }
     if (done) {
         file.print("{s}\n{s}", .{ ren_src, txt[mark.?..] });
     } else {
-        const fmt =
-            \\{s}
-            \\//->> zigem publish #|{s}|#
-            \\
-            \\//->> generated source code -- do not modify
-            \\//->> all of these lines can be safely deleted
-            \\
-        ;
-        file.print(fmt, .{ ren_src, hbuf });
+        file.print("{s}\n{s}//->> zigem publish #|{s}|#\n", .{ ren_src, tab, hbuf });
         genDecls();
+        file.print("\n{s}//->> zigem publish -- end of generated code\n", .{tab});
     }
     file.close();
 }
 
-fn genDecls() void {
+fn findDecl(dname: []const u8) Ast.Node.Index {
     for (ast.rootDecls()) |idx| {
+        const decl = ast.nodes.get(idx);
+        if (std.mem.eql(u8, ast.tokenSlice(decl.main_token + 1), dname)) return idx;
+    }
+    return 0;
+}
+
+fn genDecls() void {
+    for (members) |idx| {
         const node = ast.nodes.get(idx);
         switch (node.tag) {
             .simple_var_decl => {
@@ -67,8 +84,17 @@ fn genDecls() void {
     }
 }
 
+fn mkLines(txt: []const u8) !std.ArrayList([]const u8) {
+    var res = std.ArrayList([]const u8).init(Heap.get());
+    var iter = std.mem.splitScalar(u8, txt, '\n');
+    while (iter.next()) |s| {
+        try res.append(s);
+    }
+    return res;
+}
+
 fn walkScope(sname: []const u8, idx: u32) void {
-    file.print("\n//->> {s} publics\n", .{sname});
+    file.print("\n{s}//->> {s} publics\n", .{ tab, sname });
     const node = ast.nodes.get(idx);
     var scope: std.zig.Ast.full.ContainerDecl = undefined;
     switch (node.tag) {
@@ -86,6 +112,6 @@ fn walkScope(sname: []const u8, idx: u32) void {
         if (!std.mem.eql(u8, ast.tokenSlice(mem_decl.main_token - 1), "pub")) continue;
         const dname = ast.tokenSlice(mem_decl.main_token + 1);
         if (std.mem.startsWith(u8, dname, "em__")) continue;
-        file.print("pub const {0s} = {1s}.{0s};\n", .{ dname, sname });
+        file.print("{2s}pub const {0s} = {1s}.{0s};\n", .{ dname, sname, tab });
     }
 }
