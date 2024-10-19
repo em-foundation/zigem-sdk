02d850dc796195bfbdf4b82bfc5e11f873fb54a3
bob frankel <bob@biosbob.biz>
2024-10-19 09:28:32 -0700

    Publisher: genSpec() for interfaces

--- src/Publisher.zig
+++ src/Publisher.zig
@@ -48,7 +48,9 @@ pub fn exec(path: []const u8, force: bool) !void {
         done = !force and std.mem.eql(u8, &hbuf, suf2);
     }
 
-    zigemTest();
+    // zigemTest();
+    // print("exit.\n", .{});
+    // std.process.exit(0);
 
     file = try Out.open(norm);
     defer file.close();
@@ -57,10 +59,13 @@ pub fn exec(path: []const u8, force: bool) !void {
         return;
     }
 
-    const gen_idx = findDecl("em__generateS");
+    const kind = findUnitKind();
+
     var src_hd: []const u8 = src_pre;
     var src_tl: []const u8 = "";
-    if (gen_idx > 0) {
+
+    if (kind == .template) {
+        const gen_idx = findDecl("em__generateS");
         const gen_fn = ast.nodes.get(gen_idx);
         const gen_body = ast.nodes.get(gen_fn.data.rhs);
         const gen_blk = ast.nodes.get(gen_body.data.lhs);
@@ -74,9 +79,13 @@ pub fn exec(path: []const u8, force: bool) !void {
     }
 
     file.print("{s}\n{s}//->> zigem publish #|{s}|#\n", .{ src_hd, tab, hbuf });
-    genDecls();
+    if (kind == .interface) try genSpec() else genDecls();
     file.print("\n{s}//->> zigem publish -- end of generated code\n", .{tab});
     file.print("{s}", .{src_tl});
+}
+
+fn astNode(idx: Ast.Node.Index) Ast.Node {
+    return ast.nodes.get(idx);
 }
 
 fn findDecl(dname: []const u8) Ast.Node.Index {
@@ -87,12 +96,12 @@ fn findDecl(dname: []const u8) Ast.Node.Index {
     return 0;
 }
 
-fn findUnitKind() ?UnitKind {
+fn findUnitKind() UnitKind {
     const ud = ast.simpleVarDecl(ast.rootDecls()[1]);
     const init = ast.callFull(ud.ast.init_node);
     const fe = astNode(init.ast.fn_expr);
     const id = ast.tokenSlice(fe.data.rhs);
-    return kindMap.get(id);
+    return kindMap.get(id).?;
 }
 
 fn genDecls() void {
@@ -115,6 +124,67 @@ fn genDecls() void {
     }
 }
 
+fn genSpec() !void {
+    const SCOPE: []const []const u8 = &.{ "EM__META", "EM__TARG" };
+    const TAB4 = "    ";
+    var fn_list = std.ArrayList([]const u8).init(Heap.get());
+    for (SCOPE) |sname| {
+        const sidx = findDecl(sname);
+        if (sidx == 0) continue;
+        const cidx = astNode(sidx).data.rhs;
+        const node = astNode(cidx);
+        var scope: std.zig.Ast.full.ContainerDecl = undefined;
+        switch (node.tag) {
+            .container_decl, .container_decl_trailing => {
+                scope = ast.containerDecl(cidx);
+            },
+            .container_decl_two, .container_decl_two_trailing => {
+                var buf: [2]Ast.Node.Index = undefined;
+                scope = ast.containerDeclTwo(&buf, cidx);
+            },
+            else => return,
+        }
+        //
+        for (scope.ast.members) |fld_idx| {
+            var buf: [1]Ast.Node.Index = undefined;
+            const fld = astNode(fld_idx);
+            const fname = ast.tokenSlice(fld.main_token);
+            file.print("\nfn {s} (", .{fname});
+            try fn_list.append(fname);
+            const fn_proto = ast.fnProtoSimple(&buf, fld.data.lhs);
+            var iter = fn_proto.iterate(&ast);
+            var param_list = std.ArrayList(Ast.full.FnProto.Param).init(Heap.get());
+            while (iter.next()) |par| try param_list.append(par);
+            var sep: []const u8 = "";
+            for (param_list.items) |par| {
+                const pn = ast.tokenSlice(par.name_token.?);
+                const pt = ast.getNodeSource(par.type_expr);
+                file.print("{s}{s}: {s}", .{ sep, pn, pt });
+                sep = ", ";
+            }
+            file.print(") {s} {{\n{s}// TODO\n", .{ ast.getNodeSource(fn_proto.ast.return_type), TAB4 });
+            for (param_list.items) |par| {
+                const pn = ast.tokenSlice(par.name_token.?);
+                file.print("{s}_ = {s};\n", .{ TAB4, pn });
+            }
+            const rt = ast.getNodeSource(fn_proto.ast.return_type);
+            file.print("{s}return em.std.mem.zeroes({s});\n}}\n", .{ TAB4, rt });
+        }
+    }
+    //
+    file.print(
+        \\
+        \\const em__Self = @This();
+        \\
+        \\pub const EM__SPEC = struct {{
+        \\
+    , .{});
+    for (fn_list.items) |fname| {
+        file.print("{1s}pub const {0s} = em__Self.{0s};\n", .{ fname, TAB4 });
+    }
+    file.print("}};\n", .{});
+}
+
 fn mkLines(txt: []const u8) !std.ArrayList([]const u8) {
     var res = std.ArrayList([]const u8).init(Heap.get());
     var iter = std.mem.splitScalar(u8, txt, '\n');
@@ -124,7 +194,36 @@ fn mkLines(txt: []const u8) !std.ArrayList([]const u8) {
     return res;
 }
 
-fn walkScope(sname: []const u8, idx: u32) void {
+fn walkFields(idx: Ast.Node.Index) void {
+    const node = astNode(idx);
+    var scope: std.zig.Ast.full.ContainerDecl = undefined;
+    switch (node.tag) {
+        .container_decl, .container_decl_trailing => {
+            scope = ast.containerDecl(idx);
+        },
+        .container_decl_two, .container_decl_two_trailing => {
+            var buf: [2]Ast.Node.Index = undefined;
+            scope = ast.containerDeclTwo(&buf, idx);
+        },
+        else => return,
+    }
+    for (scope.ast.members) |fld_idx| {
+        var buf: [1]Ast.Node.Index = undefined;
+        const fld = astNode(fld_idx);
+        const fname = ast.tokenSlice(fld.main_token);
+        print("fn {s}\n", .{fname});
+        const fn_proto = ast.fnProtoSimple(&buf, fld.data.lhs);
+        var iter = fn_proto.iterate(&ast);
+        while (iter.next()) |par| {
+            const pn = ast.tokenSlice(par.name_token.?);
+            const pt = ast.getNodeSource(par.type_expr);
+            print("    {s}: {s}\n", .{ pn, pt });
+        }
+        print("ret {s}\n", .{ast.getNodeSource(fn_proto.ast.return_type)});
+    }
+}
+
+fn walkScope(sname: []const u8, idx: Ast.Node.Index) void {
     file.print("\n{s}//->> {s} publics\n", .{ tab, sname });
     const node = ast.nodes.get(idx);
     var scope: std.zig.Ast.full.ContainerDecl = undefined;
@@ -139,7 +238,7 @@ fn walkScope(sname: []const u8, idx: u32) void {
         else => return,
     }
     for (scope.ast.members) |mem_idx| {
-        const mem_decl = ast.nodes.get(mem_idx);
+        const mem_decl = astNode(mem_idx);
         if (!std.mem.eql(u8, ast.tokenSlice(mem_decl.main_token - 1), "pub")) continue;
         const dname = ast.tokenSlice(mem_decl.main_token + 1);
         if (std.mem.startsWith(u8, dname, "em__")) continue;
@@ -147,17 +246,11 @@ fn walkScope(sname: []const u8, idx: u32) void {
     }
 }
 
-fn astNode(idx: Ast.Node.Index) Ast.Node {
-    return ast.nodes.get(idx);
-}
-
 fn zigemTest() void {
-    print("{any}\n", .{findUnitKind()});
-
-    // print("{s}\n", .{ast.getNodeSource(init.ast.fn_expr)});
-    //const src = ast.getNodeSource(astNode(init.ast.fn_expr).data.rhs);
-    // print("{s}\n", .{ast.tokenSlice(id.main_token + 1)});
-
-    print("exit.\n", .{});
-    std.process.exit(0);
+    if (findUnitKind()) |kind| {
+        if (kind != .interface) return;
+    }
+    const idx = findDecl("EM__META");
+    if (idx == 0) return;
+    walkFields(astNode(idx).data.rhs);
 }
