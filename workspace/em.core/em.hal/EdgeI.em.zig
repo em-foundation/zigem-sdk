444888f9dff868282ba0252df630f0e476ea8ad6
bob frankel <bob@biosbob.biz>
2024-10-20 07:05:25 -0700

    EdgeI: select GpioI methods

--- workspace/em.core/em.hal/EdgeI.em.zig
+++ workspace/em.core/em.hal/EdgeI.em.zig
@@ -1,5 +1,5 @@
 pub const em = @import("../../zigem/em.zig");
-pub const em__U = em.interface(@This());
+pub const em__U = em.interface(@This(), .{});
 
 pub const HandlerFxn = em.Fxn(HandlerArg);
 pub const HandlerArg = struct {};
@@ -11,13 +11,15 @@ pub const EM__META = struct {
 pub const EM__TARG = struct {
     clearDetect: fn () void,
     disableDetect: fn () void,
-    getState: fn () bool,
     enableDetect: fn () void,
+    getState: fn () bool,
+    init: fn (pullup: bool) void,
     setDetectFallingEdge: fn () void,
     setDetectRisingEdge: fn () void,
 };
 
-//->> zigem publish #|d0428b25dcecca2b6f68a4b2337c6e1a8f74945d2961ca23ccb9d2fd3003f2cc|#
+
+//->> zigem publish #|e60d4295a05c95e33399c031fb0827d8cbf7bf2aaea1e26f04a16ef9dbfaa599|#
 
 pub fn setDetectHandlerM (h: HandlerFxn) void {
     // TODO
@@ -35,13 +37,19 @@ pub fn disableDetect () void {
     return em.std.mem.zeroes(void);
 }
 
+pub fn enableDetect () void {
+    // TODO
+    return em.std.mem.zeroes(void);
+}
+
 pub fn getState () bool {
     // TODO
     return em.std.mem.zeroes(bool);
 }
 
-pub fn enableDetect () void {
+pub fn init (pullup: bool) void {
     // TODO
+    _ = pullup;
     return em.std.mem.zeroes(void);
 }
 
@@ -61,8 +69,9 @@ pub const EM__SPEC = struct {
     setDetectHandlerM: *const @TypeOf(em__Self.setDetectHandlerM) = &em__Self.setDetectHandlerM,
     clearDetect: *const @TypeOf(em__Self.clearDetect) = &em__Self.clearDetect,
     disableDetect: *const @TypeOf(em__Self.disableDetect) = &em__Self.disableDetect,
-    getState: *const @TypeOf(em__Self.getState) = &em__Self.getState,
     enableDetect: *const @TypeOf(em__Self.enableDetect) = &em__Self.enableDetect,
+    getState: *const @TypeOf(em__Self.getState) = &em__Self.getState,
+    init: *const @TypeOf(em__Self.init) = &em__Self.init,
     setDetectFallingEdge: *const @TypeOf(em__Self.setDetectFallingEdge) = &em__Self.setDetectFallingEdge,
     setDetectRisingEdge: *const @TypeOf(em__Self.setDetectRisingEdge) = &em__Self.setDetectRisingEdge,
 };
