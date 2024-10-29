8a23cf3b3e67430b2e1054f825dcd260c6e44955
bob frankel <bob@biosbob.biz>
2024-10-29 09:26:37 -0700

    BleAdvTest: latest APIs

--- workspace/ti.cc23xx/ti.radio.cc23xx/BleAdvTest.em.zig
+++ workspace/ti.cc23xx/ti.radio.cc23xx/BleAdvTest.em.zig
@@ -15,18 +15,19 @@ pub const RadioDriver = em.import.@"ti.radio.cc23xx/RadioDriver";
 
 pub const EM__META = struct {
     pub fn em__configureM() void {
-        RadioConfig.phy.set(.BLE_1M);
+        RadioConfig.c_phy.setM(.BLE_1M);
     }
     pub fn em__constructM() void {
-        em__C.txTicker.set(TickerMgr.createH());
+        em__C.txTicker.setM(TickerMgr.createM());
     }
 };
 
 pub const EM__TARG = struct {
     //
-    const txTicker = em__C.txTicker;
+    const txTicker = em__C.txTicker.unwrap();
 
     var data = [_]u32{ 0x02030014, 0x0E220001, 0xBBBBCCCC, 0x0804AAAA, 0x0247495A, 0x00000601 };
+    var pkt = [_]u8{ 0x22, 0x0E, 0xCC, 0xCC, 0xBB, 0xBB, 0xAA, 0xAA, 0x04, 0x08, 0x5A, 0x49, 0x47, 0x02, 0x01, 0x06 };
 
     pub fn em__run() void {
         txTicker.start(256, &txTickCb);
@@ -36,10 +37,9 @@ pub const EM__TARG = struct {
     fn txTickCb(_: TickerMgr.CallbackArg) void {
         AppLed.wink(5);
         RadioDriver.enable();
-        RadioDriver.putWords(&data);
         var chan: u8 = 37;
         while (chan < 40) : (chan += 1) {
-            RadioDriver.startTx(chan, 5);
+            RadioDriver.startTx(&pkt, chan, 5);
             RadioDriver.waitReady();
         }
         RadioDriver.disable();
@@ -47,7 +47,7 @@ pub const EM__TARG = struct {
 };
 
 
-//->> zigem publish #|8f12d21e4fe86cc7c5d102d462728669eb2109b304be8d936644cc9281042ca2|#
+//->> zigem publish #|b00bf81eaa258b524ce83f65195541cf3cb3a6c4161ce35a4bbde2367176cb91|#
 
 //->> EM__META publics
 
