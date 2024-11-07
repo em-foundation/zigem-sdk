5cb2df9e9691bcf3e1a1f185d00f9b12557f2cd8
bob frankel <bob@biosbob.biz>
2024-11-07 10:11:28 -0800

    ActiveRunnerP: print ITERATIONS, usecs

--- workspace/em.core/em.coremark/ActiveRunnerP.em.zig
+++ workspace/em.core/em.coremark/ActiveRunnerP.em.zig
@@ -1,6 +1,7 @@
 pub const em = @import("../../zigem/em.zig");
 pub const em__U = em.module(@This(), .{});
 
+pub const Common = em.import.@"em.mcu/Common";
 pub const CoreBench = em.import.@"em.coremark/CoreBench";
 pub const Utils = em.import.@"em.coremark/Utils";
 
@@ -14,20 +15,23 @@ pub const EM__TARG = struct {
 
     pub fn em__run() void {
         em.@"%%[d+]"();
+        Common.UsCounter.start();
         for (0..ITERATIONS) |_| {
             _ = CoreBench.run(0);
         }
         em.@"%%[d-]"();
-        em.print("z: list crc = {x:0>4}\n", .{Utils.getCrc(.LIST)});
-        em.print("z: matrix crc = {x:0>4}\n", .{Utils.getCrc(.MATRIX)});
-        em.print("z: state crc = {x:0>4}\n", .{Utils.getCrc(.STATE)});
-        em.print("z: final crc = {x:0>4}\n", .{Utils.getCrc(.FINAL)});
+        const us = Common.UsCounter.stop(null);
+        em.print("{d} iterations, usecs = {d}\n", .{ ITERATIONS, us });
+        em.print("list crc = {x:0>4}\n", .{Utils.getCrc(.LIST)});
+        em.print("matrix crc = {x:0>4}\n", .{Utils.getCrc(.MATRIX)});
+        em.print("state crc = {x:0>4}\n", .{Utils.getCrc(.STATE)});
+        em.print("final crc = {x:0>4}\n", .{Utils.getCrc(.FINAL)});
     }
 };
 
-//->> zigem publish #|ab691aa544a06b13d8e0a14291cb0e0ec1d034376d96dfc16ce7b5cdb1bb746c|#
 
-//->> generated source code -- do not modify
-//->> all of these lines can be safely deleted
+//->> zigem publish #|25f5fa9778df1ff86b6b50c3639b69b695098745bb786d44c26782f8a7b81cc7|#
 
 //->> EM__TARG publics
+
+//->> zigem publish -- end of generated code
