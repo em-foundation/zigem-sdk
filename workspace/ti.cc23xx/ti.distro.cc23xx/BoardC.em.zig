f80938c93b1ea46988c16ee237a826d795bd2ca2
bob frankel <bob@biosbob.biz>
2024-11-07 07:56:06 -0800

    BoardC: useSoftUart

--- workspace/ti.cc23xx/ti.distro.cc23xx/BoardC.em.zig
+++ workspace/ti.cc23xx/ti.distro.cc23xx/BoardC.em.zig
@@ -4,7 +4,6 @@ pub const em__U = em.composite(@This(), .{});
 pub const BoardInfoC = em.import.@"em.lang/BoardInfoC";
 
 pub const AlarmMgr = em.import.@"em.utils/AlarmMgr";
-pub const AppOutUart = em.import.@"ti.mcu.cc23xx/ConsoleUart0";
 pub const BoardController = em.import.@"em.utils/BoardController";
 pub const BusyWait = em.import.@"ti.mcu.cc23xx/BusyWait";
 pub const Common = em.import.@"em.mcu/Common";
@@ -43,6 +42,7 @@ pub const SysLedPin = em__U.Generate("SysLedPin", GpioT, .{});
 
 pub const BoardInfo = struct {
     activeLowLed: bool = false,
+    useSoftUart: bool = false,
     Pin_AppBut: i16 = -1,
     Pin_AppLed: i16 = -1,
     Pin_AppOut: i16 = -1,
@@ -74,17 +74,21 @@ pub fn LP_EM_CC2340R5(b: *BoardInfo) void {
 
 pub fn LP_EM_CC2340R5_HUB(b: *BoardInfo) void {
     LP_EM_CC2340R5(b);
+    b.useSoftUart = true;
     b.Pin_AppOut = 0;
 }
 
+const BOARD: BoardInfo = BoardInfoC.initFrom(em__U.This);
+
+pub const AppOutUart = if (BOARD.useSoftUart) em.import.@"em.utils/SoftUart" else em.import.@"ti.mcu.cc23xx/ConsoleUart0";
+
 pub fn em__configureM() void {
-    const brd: BoardInfo = BoardInfoC.initFrom(em__U.This);
     AlarmMgr.x_WakeupTimer.setM(WakeupTimer);
     AppBut.x_Edge.setM(AppButEdge);
-    AppButEdge.c_pin.setM(brd.Pin_AppBut);
-    AppLedPin.c_pin.setM(brd.Pin_AppLed);
+    AppButEdge.c_pin.setM(BOARD.Pin_AppBut);
+    AppLedPin.c_pin.setM(BOARD.Pin_AppLed);
     AppLed.x_Pin.setM(AppLedPin);
-    AppOutPin.c_pin.setM(brd.Pin_AppOut);
+    AppOutPin.c_pin.setM(BOARD.Pin_AppOut);
     AppOutUart.x_TxPin.setM(AppOutPin);
     BoardController.x_Led.setM(SysLed);
     Common.x_BusyWait.setM(BusyWait);
@@ -95,10 +99,10 @@ pub fn em__configureM() void {
     Common.x_MsCounter.setM(MsCounter);
     Common.x_Uptimer.setM(Uptimer);
     Common.x_UsCounter.setM(UsCounter);
-    DbgA.c_pin.setM(brd.Pin_DbgA);
-    DbgB.c_pin.setM(brd.Pin_DbgB);
-    DbgC.c_pin.setM(brd.Pin_DbgC);
-    DbgD.c_pin.setM(brd.Pin_DbgD);
+    DbgA.c_pin.setM(BOARD.Pin_DbgA);
+    DbgB.c_pin.setM(BOARD.Pin_DbgB);
+    DbgC.c_pin.setM(BOARD.Pin_DbgC);
+    DbgD.c_pin.setM(BOARD.Pin_DbgD);
     Debug.x_DbgA.setM(DbgA);
     Debug.x_DbgB.setM(DbgB);
     Debug.x_DbgC.setM(DbgC);
@@ -107,16 +111,16 @@ pub fn em__configureM() void {
     ExtFlashDisabler.x_CS.setM(FlashCS);
     ExtFlashDisabler.x_PICO.setM(FlashPICO);
     ExtFlashDisabler.x_POCI.setM(FlashPOCI);
-    FlashCLK.c_pin.setM(brd.Pin_FlashCLK);
-    FlashCS.c_pin.setM(brd.Pin_FlashCS);
-    FlashPICO.c_pin.setM(brd.Pin_FlashPICO);
-    FlashPOCI.c_pin.setM(brd.Pin_FlashPOCI);
+    FlashCLK.c_pin.setM(BOARD.Pin_FlashCLK);
+    FlashCS.c_pin.setM(BOARD.Pin_FlashCS);
+    FlashPICO.c_pin.setM(BOARD.Pin_FlashPICO);
+    FlashPOCI.c_pin.setM(BOARD.Pin_FlashPOCI);
     Poller.x_OneShot.setM(OneShot);
-    SysLedPin.c_pin.setM(brd.Pin_SysLed);
+    SysLedPin.c_pin.setM(BOARD.Pin_SysLed);
     SysLed.x_Pin.setM(SysLedPin);
 }
 
 
-//->> zigem publish #|a955c565543691ef7c9cb46812470d06c402b70796db82541bc750b68a0d98ca|#
+//->> zigem publish #|9e3a113be137c1093b1a02e0b243cd48bf7e8ec478afe8678e912038cdfcd630|#
 
 //->> zigem publish -- end of generated code
