5fcce638bb4d75fbcc5d634a947801f0a58ddd18
Geoffrey Cohler <g.cohler@computer.org>
2024-11-14 11:31:10 -0500

    Improve testing:  Add -b (build only) and -p (promptBetweenTests) switches (#49)

--- utils/combo_tests.sh
+++ utils/combo_tests.sh
@@ -2,15 +2,68 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+buildOnly=
+promptBetween=
+
+Help()
+{
+   # Display Help
+   echo "Run basic end-to-end tests"
+   echo
+   echo "options:"
+   echo "b     Build only -- do not load/run"
+   echo "p     Prompt between tests"
+   echo "h     Print this Help."
+   echo
+}
+
+RunTest() {
+  title="(build, load, and run)"
+  load="-l"
+  if [ "${3}" != "" ]; then
+    load=
+    title="(build only)"
+  fi
+  printf "\n${1} ${title}\n"
+  zigem compile -f ${1} ${load} | grep 'image size:'
+  if [ "${3}" == "" ]; then
+    printf "${2}"
+  fi
+  if [ "${4}" != "" ]; then
+    read -p ">>> Once ready, press enter to continue"
+  fi
+}
+
+while getopts ":hbp" option; do
+   case $option in
+      h)
+         Help
+         exit
+         ;;
+      b)
+         buildOnly="-b"
+         load=""
+         ;;
+      p)
+         promptBetween="-p"
+         ;;
+     \?)
+         echo "Error: Invalid option"
+         exit
+         ;;
+   esac
+done
+
 pushd ${SCRIPT_DIR}/../workspace > /dev/null
 
-printf "\nBe sure that CC2340 LaunchPad is connected via XDS-100\n"
-printf "Also start SerialMonitor listening to the XDS-100 port at 115200,8n1n\n\n"
-read -p "Once ready, press enter to start tests"
+if [ "$buildOnly" == "" ]; then
+  printf "\nBe sure that CC2340 LaunchPad is connected via XDS-100\n"
+  printf "Also start SerialMonitor listening to the XDS-100 port at 115200,8n1n\n\n"
+  if [ "$promptBetween" != "" ]; then
+    read -p ">>> Once ready, press enter to start tests"
+  fi
+fi
 
-printf "\nCombo/Ex01_TickerP -- you should see occasional blinks of the red and green LEDs\n"
-printf "... and button clicks should change the rate ... and printout on the serial port\n"
-zigem compile -f em.core/em.examples.combo/Ex01_TickerP.em.zig -l
-read -p "Once ready, press enter to exit the tests"
+RunTest "em.core/em.examples.combo/Ex01_TickerP.em.zig" "    you should see occasional blinks of the red and green LEDs\n    and button clicks should change the rate\n    and printout on the serial port\n" "$buildOnly" "$promptBetween"
 
 popd > /dev/null
--- utils/install.sh
+++ utils/install.sh
@@ -4,7 +4,18 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 pushd ${SCRIPT_DIR}/../ > /dev/null
 
+printf "\nInstalling zigem\n"
 zig build
+
+printf "\nVerifying zigem installation\n"
 zig build verify
+
+latestVsix=$(ls zig-out/tools/vscode-zigem*.vsix | tail -n 1)
+if [ "$latestVsix" != "" ]; then
+  printf "\nInstall zigem vscode extension\n"
+  code --install-extension $latestVsix
+else
+  printf "\nNo vscode extension found in zig-out/tools\n"
+fi
 
 popd > /dev/null
